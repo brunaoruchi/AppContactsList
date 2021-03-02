07f428675e4f4ce787863d57d8c86e1f5cbde541
brunaoruchi <brunaoruchi@alunos.utfpr.edu.br>
2021-03-02 20:23:46 -0300

    Adiciona função para renderizar uma lista

--- App.js
+++ App.js
@@ -4,10 +4,20 @@ import {Text, View} from 'react-native';
 import Header from './src/components/Header';
 
 export default class App extends Component{
+  renderList(){
+    const names = ['José', 'João', 'Maria', 'Pedro'];
+
+    const textElements = names.map(name =>{
+      return <Text>{name}</Text>
+    });
+    return textElements;
+  }
+
   render() {
     return (
       <View>
         <Header label={'Contatos'} />
+        { this.renderList() }
       </View>
     );
   }
