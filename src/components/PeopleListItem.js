542b248cd47088e9540f06de4f402fbcba94aef7
brunaoruchi <brunaoruchi@alunos.utfpr.edu.br>
2021-03-05 17:21:29 -0300

    Adiciona style na Image

--- src/components/PeopleListItem.js
+++ src/components/PeopleListItem.js
@@ -26,11 +26,13 @@ const styles = StyleSheet.create({
   lineText: {
     fontSize: 20,
     paddingLeft: 15,
+    flex: 7
   },
   avatar:{
     aspectRatio: 1,
-    width: 50
-
+    flex: 1,
+    marginLeft: 15,
+    borderRadius: 5
   }
 })
 
