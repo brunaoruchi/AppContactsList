9a3758d7a2c5f03a5a899a46bc6742c2258b028d
brunaoruchi <brunaoruchi@alunos.utfpr.edu.br>
2021-03-05 19:46:00 -0300

    Adiciona style na screen DetailPerson

--- src/screens/PersonDetail.js
+++ src/screens/PersonDetail.js
@@ -10,32 +10,39 @@ export default class PersonDetail extends React.Component {
     return(
       <View style={styles.container}>
         <Image source={{uri: picURL}} style={styles.avatar}/>
-        <Text>
-          Olá, página de detalhes :)
-        </Text>
+        <View style={styles.detailContainer}>
+          <View style={styles.line}>
+            <Text style={styles.cell}>E-mail: </Text>
+            <Text style={styles.cell}>{person.email}</Text>
+          </View>
+        </View>
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  // line: {
-  //   height: 60,
-  //   borderBottomWidth: 1,
-  //   borderBottomColor: '#bbb',
-  //   alignItems:'center',
-  //   flexDirection: 'row',
-  // },
-  // lineText: {
-  //   fontSize: 20,
-  //   paddingLeft: 15,
-  //   flex: 7
-  // },
   container:{
     padding: 15
   },
   avatar:{
     aspectRatio: 1,
     borderRadius: 5
+  },
+  detailContainer: {
+    backgroundColor: '#E2F9FF',
+    marginTop: 20,
+    elevation: 1,
+    flexDirection: 'row'
+  },
+  line: {
+    flexDirection: 'row',
+    paddingTop: 3,
+    borderWidth: 1,
+    borderColor: "#C5C5C5"
+  },
+  cell:{
+    paddingLeft: 5,
+    fontSize: 16,
   }
 })
