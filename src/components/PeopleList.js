2511232329ecdfae3c0b41c749e5964e75a05d91
brunaoruchi <brunaoruchi@alunos.utfpr.edu.br>
2021-03-05 16:40:52 -0300

    Adiciona style no componente PeopleList

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -1,18 +1,41 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 const PeopleList = props => {
   const {people} = props;
 
   const textElements = people.map(person => {
     const {first} = person.name;
-    return <Text key={first}>{first}</Text>
+    return(
+      <View style={styles.line} key={first}>
+        <Text style={styles.lineText}>{first}</Text>
+      </View>
+    );
   })
+
   return(
-    <View>
+    <View style={styles.container}>
       {textElements}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#e2f9ff',
+  },
+  line: {
+    height: 60,
+    borderBottomWidth: 1,
+    borderBottomColor: '#bbb',
+    alignItems:'center',
+    flexDirection: 'row',
+  },
+  lineText: {
+    fontSize: 20,
+    paddingLeft: 15,
+  }
+})
+
 
 export default PeopleList;
