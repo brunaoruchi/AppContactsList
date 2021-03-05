97e41a24bd6b52b08165f6dec96c980dca82ca2c
brunaoruchi <brunaoruchi@alunos.utfpr.edu.br>
2021-03-05 17:17:01 -0300

    Adiciona o Image da API

--- src/components/PeopleListItem.js
+++ src/components/PeopleListItem.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 import capitalizeFirstLetter from '../util/capitalizeFirstLetter';
 
 const PeopleListItem = props => {
   const {person} = props;
   const {title, first, last} = person.name;
+  const {thumbnail} = person.picture;
 
   return(
     <View style={styles.line}>
+      <Image source={{uri: thumbnail}} style={styles.avatar}/>
       <Text style={styles.lineText}>{capitalizeFirstLetter(title) + " " + capitalizeFirstLetter(first) + " " + capitalizeFirstLetter(last)}</Text>
     </View>
   )
@@ -24,6 +26,11 @@ const styles = StyleSheet.create({
   lineText: {
     fontSize: 20,
     paddingLeft: 15,
+  },
+  avatar:{
+    aspectRatio: 1,
+    width: 50
+
   }
 })
 
