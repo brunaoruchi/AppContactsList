beaef823dab2fd9ce866bf7edaeca4632f7b378a
brunaoruchi <brunaoruchi@alunos.utfpr.edu.br>
2021-03-05 16:17:25 -0300

    Adiciona o RenderList

--- App.js
+++ App.js
@@ -4,15 +4,35 @@ import Header from './src/components/Header';
 import axios from 'axios';
 
 export default class App extends Component{
-  renderList(){
+  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      people: []
+    };
+  }
+  
+  //É chamado apenas uma única vez quando renderizado a tela
+  componentDidMount(){
     axios
     .get('https://randomuser.me/api/?nat=br&results=5')
     .then(response => {
       //destruct = recupera cada usuario
       const {results} = response.data;
-      const names = results.map(person => person.name.first);
-      console.log(names);
+      this.setState({
+        people: results
+      });
     });
+  }
+
+  renderList(){
+    const textElements = this.state.people.map(person => {
+      const {first} = person.name;
+
+      return <Text key={first}>{first}</Text>
+    })
+    return textElements;
   }
 
   render() {
