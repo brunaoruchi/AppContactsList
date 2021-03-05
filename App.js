c7c48a7c6a99780edab8db8d5a835be1c34d4edd
brunaoruchi <brunaoruchi@alunos.utfpr.edu.br>
2021-03-05 18:40:50 -0300

    Adiciona ao package navigation, gesture-handler e realiza o link para navegação

--- App.js
+++ App.js
@@ -1,3 +1,13 @@
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+
 import PeoplePage from './src/screens/PeoplePage';
+
+const StackNavigator = createStackNavigator({
+  'Main':{
+    screen: PeoplePage
+  }
+})
+
+const AppContainer = createAppContainer(StackNavigator);
 
 export default PeoplePage;
